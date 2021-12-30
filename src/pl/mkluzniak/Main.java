9aa6d772dde5368a2178c877a2ba7f7070c4392e
Marceli Kluzniak <marcelikluzniak5@gmail.com>
2021-12-30 16:18:20 +0100

    Initial commit

---
+++ src/pl/mkluzniak/Main.java
@@ -0,0 +1,13 @@
+package pl.mkluzniak;
+
+import java.awt.event.ActionEvent;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LaunchPage launchPage = new LaunchPage();
+
+
+    }
+}
